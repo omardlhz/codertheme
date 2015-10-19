8f1fab26445ea51aa53eec1c28f9fd924e4a7a10
Omar Dlhz <omar.dlhz@hotmail.com>
2015-10-18 22:40:45 -0400

    Fixed the link to each posts date.

--- index.php
+++ index.php
@@ -3,12 +3,22 @@
 
 if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
+
+
+	<?php
+
+		$sourceUrl = parse_url($url);
+		$sourceUrl = $sourceUrl['host'];
+	?>
+	
+	<script> alert(<?php echo $sourceUrl ?>): </script>
+
 	<article class="post">
 	<h2><a class="postTitle" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<div class="theText">
 	<?php the_excerpt(); ?>
 	<div class="readWrapper"><a class="theLink" href="<?php the_permalink(); ?>">Read More...</a></div>
-	<div class="dateWrapper"><a class ="theLink" href="<?php site_url() . the_time('Y/m/d');?>"><?php the_time('F j, Y'); ?></a></div>
+	<div class="dateWrapper"><a class ="theLink" href="<?php echo 'http://'. $_SERVER['HTTP_HOST']. "/" ?><?php echo the_time('Y/m/d');?>"><?php the_time('F j, Y'); ?></a></div>
 	</div>
 	</article>
 	<?php endwhile; ?>
