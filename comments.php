43ce5cbf003017ef8b1611e9e964b9009fce5b06
Omar Dlhz <omar.dlhz@hotmail.com>
2015-12-26 14:37:55 -0500

    Comments.php updated, comments are displayed

--- comments.php
+++ comments.php
@@ -19,14 +19,34 @@
 			<input class="textStyle" placeholder="Name <?php if($req) echo "(required)"; ?>" type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" />
 			<input class="textStyle" placeholder="Mail <?php if($req) echo "(required)"; ?>" type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" />
 		<?php endif; ?>
-
-		<p><textarea class="commentArea" rows="10" name="comment" id="comment"></textarea>
+		<p><textarea class="commentArea" placeholder="Add a public comment..." rows="10" name="comment" id="comment"></textarea>
 		<input class="myButton" name="submit" type="submit" id="submit" tabindex="5" value="Post" />
 		<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></p>
 		<?php do_action('comment_form', $post->ID); ?>
 		</form>
 		</p>
 	<?php endif; ?>
+	<br />
+	<?php if($comments) : ?>
+	  	<?php foreach($comments as $comment) : ?>
+				<div style="margin-left: 70px; margin-bottom: 25px;">
+				  <div style="float: right; width: 100%;">
+				  <?php if ($comment->comment_approved == '0') : ?>
+					<p style="width:100%;">Your comment is awaiting approval</p>
+				  <?php endif; ?>
+				  <div style="font-size: 14px; margin: 0px; padding: 0px; margin-left: 10px; font-weight: bold;"><?php comment_author_link(); ?> on <?php comment_date(); ?></div>
+				  <p style="font-size: 14px; margin: 0px; padding: 0px; margin-left: 10px;"><?php echo $comment->comment_content; ?></p>
+				  </div>
+				  <div style="float: left; width: 70px; margin-left: -70px;">
+				  <?php echo get_avatar(get_comment_author_email(), 70, $default_avatar ) ?>
+				  </div>
+				  <div style="clear: both;"></div>
+				</div>
+		<?php endforeach; ?>
+	<?php else : ?>
+		<p>No comments yet</p>
+	<?php endif; ?>
+
 <?php else: ?>
 
 	<p>The comments are closed.</p>
